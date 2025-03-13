66aad403d0a96e6e6bf6304f9603a46e4f0093db
SamuelStandcumbe <sstandcumbe@gmail.com>
2025-03-13 11:25:28 +0000

    Update gtng.py

--- Python/gtng.py
+++ Python/gtng.py
@@ -1,22 +1,23 @@
+
 import random
+
 
 random_number = random.randint(1, 100)
 
-print("The computer will generate a number betwenn 1 and 100, then you will guess that number. The amount of attemptswuill be recorded, so guess carefully!\n")
+print("The computer will generate a number betwenn 1 and 100, then you will guess that number. The amount of attempts will be recorded, so guess carefully!\n")
 attempts = 0
 
-guess = int(input("Make a guess: "))
-
-while True:
-    if guess != random_number:
-        attempts += 1
-        if guess == random_number:
-                print(f"You win. It took you {attempts} attempts to guess the number.")
-                break
-        elif guess > random_number:
-            print("Too high")
-            int(input("Make another guess: "))
-        elif guess < random_number:
-            print("Too low")
-            int(input("Make another guess: "))
-
+while True:  
+    guess = int(input("Make a guess: "))
+    attempts += 1
+    
+    if guess < random_number:
+        print("Too low")
+       
+    elif guess > random_number:
+        print("Too high")
+        
+    elif guess == random_number:
+        print(f"You win. It took you {attempts} attempts to guess the number.")
+        break
+   
